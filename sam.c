8a444997cc599ab81b5be1cc9c0fe4c5bb4b83bd
Sam-1309 <samruddhijum@gmail.com>
2024-09-23 22:17:48 +0530

    C programing
    This code display how two numbers of addition work in C programing language

---
+++ sam.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+int main ()
+{
+printf ("addition of two numbers");
+//this code display how two numbers of addition work in c //
+int a;
+ printf ("enter a");
+ scanf("%d",&a);
+ int b;
+ printf("eneter b");
+ scanf("%d",&b);
+ int sum= a+b;
+ printf("sumis :%d",a+b);
+  return 0;
+}
