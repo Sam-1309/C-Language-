809d05428586cf904a3242d00a277290ac896b01
Sam-1309 <samruddhijum@gmail.com>
2024-09-25 23:25:09 +0530

    C language
     This code show how two integer numbers multiplication works in C language

---
+++ C.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+//This code show how two integer  numbers  multiplication work in C language //;
+int main () {
+    int a,b ;
+    printf ("enter a");
+    scanf ("%d",&a);
+    printf ("enter b");
+    scanf ("%d",&b);
+    int multiplication = a*b;
+    printf("multiplication is :%d",a*b);
+    return 0;
+}
