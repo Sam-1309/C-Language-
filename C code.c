96fa6a9ce3daf854f4ddc49de0d26fc727663b87
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-09-26 23:21:10 +0530

    C language
    Greatest number finding code in C language

---
+++ C code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+//This code  define greatest number in C language//
+int main() {
+    int num1,num2;
+    num1=13;
+    num2=30;
+    if (num1>num2)
+    printf("%d is greater",num1);
+    if (num1<num2)
+    printf("%d is greater",num2);
+    return 0 ;
+}
