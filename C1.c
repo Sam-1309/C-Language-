74e36f227c3709d172f1d3e35dd6509b08324078
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-09-27 21:12:55 +0530

    C language
    area of circle code in C language

---
+++ C1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> ;
+int main () { ;
+float pi=3.14;
+int r; //r is radius of circle //
+printf("enter r");
+scanf("%f",&r);
+printf("area of circle is:%f",&3.14*r*r);
+return 0;
+}
