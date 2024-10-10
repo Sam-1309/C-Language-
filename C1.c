6506a445e2ff41badbebe234e7ae3ffa02cdb7c3
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-10-10 19:31:42 +0530

    Delete C1.c

--- C1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> ;
-int main () { ;
-float pi=3.14;
-int r; //r is radius of circle //
-printf("enter r");
-scanf("%f",&r);
-printf("area of circle is:%f",&3.14*r*r);
-return 0;
-}
