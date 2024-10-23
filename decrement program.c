ee1ffa0ccafbf60e374da3d667a609ec7a9eb48a
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-10-23 21:21:14 +0530

    C

---
+++ decrement program.c
@@ -0,0 +1,10 @@
+main()
+{
+    int a=8;
+    --a;//--a is pre decrement function a=a-1//
+    printf("%d",a);
+    int s=14;
+    s--;//s-- is post decrement function s=s-1//
+    printf("%d",s);
+    return 0;
+}
