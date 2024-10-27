5654c40fa0559377a5b8b8d074b78fa75add8d10
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-10-27 20:35:30 +0530

    C
    C program by using sizeof function

---
+++ sizeof.c
@@ -0,0 +1,11 @@
+main()
+{
+    int s,t,r;
+    s=sizeof(float); // here sizeof that data type unit is in bytes //
+    printf ("%d",s);
+    t=sizeof (double);
+    printf("%d",t);
+    r=sizeof(int);
+    printf("%d",r);
+    return 0 ;
+}
