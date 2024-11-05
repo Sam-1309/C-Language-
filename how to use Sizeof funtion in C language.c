c73c05bb0e1a50708dcdf7efafc06b78f2aaca2b
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-11-05 20:22:36 +0530

    C

---
+++ how to use Sizeof funtion in C language.c
@@ -0,0 +1,13 @@
+main()
+{
+    int a,b,c,d ;
+    a=sizeof (char); //size of that data type came in bytes // //1 bytes = 8 bits //
+    printf ("%d",a);
+    b=sizeof ("case");
+    printf("%d",b);
+    c=sizeof("const");
+    printf("%d",c);
+    d=sizeof("continue");
+    printf("%d",d);
+    return 0;
+    }
