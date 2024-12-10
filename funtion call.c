2bfaa73417268ae795bc18e708a78f9dbe6b8470
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-12-10 21:09:55 +0530

    Add files via upload

---
+++ funtion call.c
@@ -0,0 +1,41 @@
+main()
+{
+    a();
+    b();
+    c();
+    d();
+}
+a()
+{
+    int a;
+    printf(" enter any numbers");
+    scanf("%d",&a);
+    if ("a%2= 0")
+    printf(" value is even");
+    else ;
+    printf("value is odd");
+    return 0;
+}
+b()
+{
+    a();
+    int b;
+    printf("enter any number");
+    scanf("%d",&b);
+    if
+        ("b%2=/0")
+         printf("value is odd");
+    else
+    printf("value even");
+    return 0;
+}
+c()
+{
+    a();
+    b();
+    }
+    d()
+    {
+        b();
+        a();
+    }
