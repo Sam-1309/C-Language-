794395a185de428f62e0ed4c9ce6fd5d10bcf3af
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2024-12-21 19:13:05 +0530

    Add files via upload

---
+++ take nothing and return nothing program.c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void  main()
+{
+void add(void);
+    add();
+}
+   void add()
+{
+    int a,b,c;
+    printf("enter any two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum is %d",c);
+
+return 0;
+}
