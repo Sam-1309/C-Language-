9184d0d2f51b49d52b0db94e05ce8561084afe76
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2025-02-13 21:40:08 +0530

    Add files via upload

---
+++ Basic arethmatic operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+    //ADDITION
+    int a,b,add,sub,multiply,division;
+    printf("any two integers",a,b);
+    scanf("%d %d",&a,&b);
+  add=a+b;
+printf("%d",add);
+//SUBTRACTION
+    printf("any two integers",a,b);
+    scanf("%d %d",&a,&b);
+  sub=a-b;
+printf("%d",sub);
+//MULTIPLICATION
+    printf("any two integers",a,b);
+    scanf("%d %d",&a,&b);
+  multiply=a*b;
+printf("%d",multiply);
+//DIVISION
+    printf("any two integers",a,b);
+    scanf("%d %d",&a,&b);
+  division=a/b;
+printf("%d",division);
+return 0;
+    }
