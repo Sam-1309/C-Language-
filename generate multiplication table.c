b66b2f1622d1958fae091a731b84290e0acd79e9
Samruddhi Jumnalkar <samruddhijum@gmail.com>
2025-02-14 21:41:34 +0530

    Add files via upload

---
+++ generate multiplication table.c
@@ -0,0 +1,27 @@
+main()
+{
+int a,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10;
+printf("any integer");
+scanf("%d",&a);
+m1=a*1;
+printf("%d",m1);
+m2=a*2;
+printf("%d",m2);
+m3=a*3;
+printf("%d",m3);
+m4=a*4;
+printf("%d",m4);
+m5=a*5;
+printf("%d",m5);
+m6=a*6;
+printf("%d",m6);
+m7=a*7;
+printf("%d",m7);
+m8=a*8;
+printf("%d",m8);
+m9=a*9;
+printf("%d",m9);
+m10=a*10;
+printf("%d",m10);
+    return 0;
+}
